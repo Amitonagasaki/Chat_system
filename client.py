0eaffbc9f987dc82cf0b2756342971045ace6326
Amito <amitlighting13@gmail.com>
2022-04-17 01:09:34 +0530

    added required files

---
+++ client.py
@@ -0,0 +1,40 @@
+from concurrent.futures import thread
+from email import message
+from pydoc import cli
+import socket
+import threading
+nickname = input("choose a nickname: ")
+
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect(('127.0.0.1',8080))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+                
+        except:
+            print("AN ERROR OCCURED")  
+            client.close()
+            break
+            
+def write():
+    while True: 
+        message = f'{nickname}:{input("")}'
+        client.send(message.encode('ascii'))
+        
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()       
+
+write_thread=threading.Thread(target=write)
+write_thread.start()
+
+                
+            
+            
+            
